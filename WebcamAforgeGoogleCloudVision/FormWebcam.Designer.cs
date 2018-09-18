3067228cbae30ce66a57fdb93e276bc1a2b6b260
Alex Almeida <alex.aldr@gmail.com>
2018-09-18 01:18:00 -0300

    Initial commit

---
+++ WebcamAforgeGoogleCloudVision/FormWebcam.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace WebcamAforgeGoogleCloudVision
+{
+    partial class FormWebcam
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormWebcam));
+            this.btSair = new System.Windows.Forms.Button();
+            this.btSalvar = new System.Windows.Forms.Button();
+            this.btAnalisar = new System.Windows.Forms.Button();
+            this.pbWebcam = new System.Windows.Forms.PictureBox();
+            this.cbResolucao = new System.Windows.Forms.ComboBox();
+            this.lblResolucao = new System.Windows.Forms.Label();
+            this.lblDispositivo = new System.Windows.Forms.Label();
+            this.cbDispositivo = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbWebcam)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btSair
+            // 
+            this.btSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btSair.Location = new System.Drawing.Point(381, 441);
+            this.btSair.Name = "btSair";
+            this.btSair.Size = new System.Drawing.Size(80, 25);
+            this.btSair.TabIndex = 17;
+            this.btSair.Text = "Sair";
+            this.btSair.UseVisualStyleBackColor = true;
+            this.btSair.Click += new System.EventHandler(this.btSair_Click);
+            // 
+            // btSalvar
+            // 
+            this.btSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btSalvar.Location = new System.Drawing.Point(295, 441);
+            this.btSalvar.Name = "btSalvar";
+            this.btSalvar.Size = new System.Drawing.Size(80, 25);
+            this.btSalvar.TabIndex = 16;
+            this.btSalvar.Text = "Salvar";
+            this.btSalvar.UseVisualStyleBackColor = true;
+            this.btSalvar.Click += new System.EventHandler(this.btSalvar_Click);
+            // 
+            // btAnalisar
+            // 
+            this.btAnalisar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btAnalisar.Location = new System.Drawing.Point(209, 441);
+            this.btAnalisar.Name = "btAnalisar";
+            this.btAnalisar.Size = new System.Drawing.Size(80, 25);
+            this.btAnalisar.TabIndex = 15;
+            this.btAnalisar.Text = "Analisar";
+            this.btAnalisar.UseVisualStyleBackColor = true;
+            this.btAnalisar.Click += new System.EventHandler(this.btAnalisar_Click);
+            // 
+            // pbWebcam
+            // 
+            this.pbWebcam.BackColor = System.Drawing.Color.Gainsboro;
+            this.pbWebcam.Location = new System.Drawing.Point(12, 35);
+            this.pbWebcam.Name = "pbWebcam";
+            this.pbWebcam.Size = new System.Drawing.Size(640, 400);
+            this.pbWebcam.TabIndex = 14;
+            this.pbWebcam.TabStop = false;
+            // 
+            // cbResolucao
+            // 
+            this.cbResolucao.FormattingEnabled = true;
+            this.cbResolucao.Location = new System.Drawing.Point(480, 8);
+            this.cbResolucao.Name = "cbResolucao";
+            this.cbResolucao.Size = new System.Drawing.Size(172, 21);
+            this.cbResolucao.TabIndex = 13;
+            this.cbResolucao.SelectedIndexChanged += new System.EventHandler(this.cbResolucao_SelectedIndexChanged);
+            // 
+            // lblResolucao
+            // 
+            this.lblResolucao.AutoSize = true;
+            this.lblResolucao.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblResolucao.Location = new System.Drawing.Point(395, 9);
+            this.lblResolucao.Name = "lblResolucao";
+            this.lblResolucao.Size = new System.Drawing.Size(79, 17);
+            this.lblResolucao.TabIndex = 12;
+            this.lblResolucao.Text = "Resolução:";
+            // 
+            // lblDispositivo
+            // 
+            this.lblDispositivo.AutoSize = true;
+            this.lblDispositivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblDispositivo.Location = new System.Drawing.Point(12, 9);
+            this.lblDispositivo.Name = "lblDispositivo";
+            this.lblDispositivo.Size = new System.Drawing.Size(80, 17);
+            this.lblDispositivo.TabIndex = 11;
+            this.lblDispositivo.Text = "Dispositivo:";
+            // 
+            // cbDispositivo
+            // 
+            this.cbDispositivo.FormattingEnabled = true;
+            this.cbDispositivo.Location = new System.Drawing.Point(98, 8);
+            this.cbDispositivo.Name = "cbDispositivo";
+            this.cbDispositivo.Size = new System.Drawing.Size(241, 21);
+            this.cbDispositivo.TabIndex = 10;
+            this.cbDispositivo.SelectedIndexChanged += new System.EventHandler(this.cbDispositivo_SelectedIndexChanged);
+            // 
+            // FormWebcam
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 476);
+            this.Controls.Add(this.btSair);
+            this.Controls.Add(this.btSalvar);
+            this.Controls.Add(this.btAnalisar);
+            this.Controls.Add(this.pbWebcam);
+            this.Controls.Add(this.cbResolucao);
+            this.Controls.Add(this.lblResolucao);
+            this.Controls.Add(this.lblDispositivo);
+            this.Controls.Add(this.cbDispositivo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormWebcam";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WebcamAforge - Google Cloud Vision API";
+            this.Load += new System.EventHandler(this.FormWebcam_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbWebcam)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btSair;
+        private System.Windows.Forms.Button btSalvar;
+        private System.Windows.Forms.Button btAnalisar;
+        private System.Windows.Forms.PictureBox pbWebcam;
+        private System.Windows.Forms.ComboBox cbResolucao;
+        private System.Windows.Forms.Label lblResolucao;
+        private System.Windows.Forms.Label lblDispositivo;
+        private System.Windows.Forms.ComboBox cbDispositivo;
+    }
+}
+
---
+++ WebcamAforgeGoogleCloudVision/FormWebcam.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video.DirectShow;      //framework adicionado pelo nuget
+using AForge.Video;                 //framework adicionado pelo nuget
+using Google.Cloud.Vision.V1;       //api adicionada pelo nuget
+using Newtonsoft.Json;              //biblioteca adicionada pelo nuget
+using System.Drawing.Imaging;
+using Google.Apis.Auth.OAuth2;
+using System.IO;
+
+namespace WebcamAforgeGoogleCloudVision
+{
+    public partial class FormWebcam : Form
+    {
+        private FilterInfoCollection dispositivos;
+        private VideoCaptureDevice dispositivo;
+        private SaveFileDialog saveFileDialog;
+        private OpenFileDialog openFileDialog;
+
+        public FormWebcam()
+        {
+            InitializeComponent();
+        }
+
+        private void FormWebcam_Load(object sender, EventArgs e)
+        {
+            AtualizarDispositivos();
+        }
+
+        private void AtualizarDispositivos()
+        {
+            dispositivos = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            if (dispositivos != null && dispositivos.Count != 0)
+            {
+                AtualizarComboDispositivos();
+                cbDispositivo.SelectedIndex = 0;
+                AtualizarComboResolucao();
+            }
+            else
+            {
+                cbDispositivo.Items.Add("Nenhum dispositivo encontrado!");
+                cbDispositivo.SelectedIndex = 0;
+            }
+        }
+
+        private void AtualizarComboDispositivos()
+        {
+            cbDispositivo.Items.Clear();
+            foreach (FilterInfo webcams in dispositivos)
+            {
+                cbDispositivo.Items.Add(webcams.Name);
+            }
+            dispositivo = new VideoCaptureDevice(dispositivos[0].MonikerString);
+        }
+
+        private void AtualizarComboResolucao()
+        {
+            if (dispositivo.VideoCapabilities.Length != 0)
+            {
+                cbResolucao.Items.Clear();
+                foreach (VideoCapabilities resolucoes in dispositivo.VideoCapabilities)
+                {
+                    cbResolucao.Items.Add(string.Format("{0} x {1}", resolucoes.FrameSize.Width, resolucoes.FrameSize.Height));
+                }
+                cbResolucao.SelectedIndex = 0;
+                AlterarComboResolucao();
+            }
+            else
+            {
+                cbDispositivo.Items.Add("Não disponível!");
+            }
+        }
+
+        private void AlterarComboDispositivo()
+        {
+            LiberarDispositivo();
+            dispositivo = new VideoCaptureDevice(dispositivos[cbDispositivo.SelectedIndex].MonikerString);
+            //linkar os métodos para clonar os frames
+            dispositivo.NewFrame += NovoFrame;
+            AtualizarComboResolucao();
+            InicializarDispositivo();
+        }
+
+        private void AlterarComboResolucao()
+        {
+            LiberarDispositivo();
+            dispositivo.VideoResolution = dispositivo.VideoCapabilities[cbResolucao.SelectedIndex];
+            InicializarDispositivo();
+        }
+
+        private void InicializarDispositivo()
+        {
+            if (!dispositivo.IsRunning)
+            {
+                dispositivo.Start();
+            }
+        }
+
+        private void LiberarDispositivo()
+        {
+            if (dispositivo.IsRunning)
+            {
+                dispositivo.Stop();
+                pbWebcam.Image = null;
+            }
+        }
+
+        private void NovoFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            if (pbWebcam.Image != null)
+            {
+                pbWebcam.Image.Dispose();
+            }
+            pbWebcam.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void cbDispositivo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            AlterarComboDispositivo();
+        }
+
+        private void cbResolucao_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            AlterarComboResolucao();
+        }
+
+        private void btSair_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja realmente sair?", "Sair do aplicativo!", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+            {
+                LiberarDispositivo();
+                Application.Exit();
+            }
+
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            LiberarDispositivo();
+            base.OnFormClosing(e);
+        }
+
+        private void btAnalisar_Click(object sender, EventArgs e)
+        {
+            using (openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Imagens (*.jpg)|*.jpg";
+                openFileDialog.DefaultExt = "*.jpg";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        lock (this)
+                        {
+                            //Variável setada no código devido erro ao reconhecer variável setada no ambiente
+                            System.Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", @"C:\ApiKeys\googleKey.json");
+                            string Pathsave = System.Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS");
+
+                            //Cliente da requisição
+                            var client = ImageAnnotatorClient.Create();
+
+                            //Carregar a imagem na memória
+                            var imagem = Google.Cloud.Vision.V1.Image.FromFile(openFileDialog.FileName);
+
+                            //Executa a análise da imagem
+                            var resposta = client.DetectFaces(imagem);
+
+                            foreach (var face in resposta)
+                            {
+                                MessageBox.Show("Imagem analisada com sucesso:\n\n\n"
+                                    + "Nome: " + openFileDialog.SafeFileName + ":\n"
+                                    + "Alegria: " + face.JoyLikelihood + "\n"
+                                    + "Raiva: " + face.AngerLikelihood + "\n"
+                                    + "Tristeza: " + face.SorrowLikelihood + "\n"
+                                    + "Surpresa: " + face.SurpriseLikelihood + "\n"
+                                    + "Subexposição: " + face.UnderExposedLikelihood + "\n"
+                                    + "Borrado: " + face.BlurredLikelihood + "\n"
+                                    + "Chapéu: " + face.HeadwearLikelihood + "\n"
+                                    + "Confiança da análise: " + face.DetectionConfidence + "\n"
+                                    + "delimitador: " + face.BoundingPoly + "\n"
+                                    + "FdBoundingPoly: " + face.FdBoundingPoly + "\n"
+                                    //+ "Landmarks: " + face.Landmarks + "\n"
+                                    //+ "LandmarkingConfidence: " + face.LandmarkingConfidence + "\n"
+                                    + "PanAngle: " + face.PanAngle + "\n"
+                                    + "RollAngle: " + face.RollAngle + "\n"
+                                    + "TiltAngle: " + face.TiltAngle + "\n"
+
+                                    + ".", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                            //MessageBox.Show("Imagem analisada com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Falha ao analisar imagem!\n" + ex.Message, "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    finally
+                    {
+
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Operação cancelada!", "Cancelado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+
+        private void btSalvar_Click(object sender, EventArgs e)
+        {
+            if (pbWebcam.Image != null)
+            {
+
+                //remover o link
+                dispositivo.NewFrame -= NovoFrame;
+
+                //using para ser descartado depois
+                using (saveFileDialog = new SaveFileDialog())
+                {
+                    saveFileDialog.Filter = "Imagens (*.jpg)|*.jpg";
+                    saveFileDialog.DefaultExt = "*.jpg";
+                    /*                     
+                    JPEG
+                    PNG8
+                    PNG24
+                    GIF
+                    GIF animado (primeiro quadro apenas)
+                    BMP
+                    WEBP
+                    RAW
+                    ICO
+                     */
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        ImageFormat format = ImageFormat.Jpeg;
+
+                        try
+                        {
+                            lock (this)
+                            {
+                                Bitmap imagem = (Bitmap)pbWebcam.Image;
+                                imagem.Save(saveFileDialog.FileName, format);
+                                MessageBox.Show("Imagem salva com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Falha ao salvar a imagem!\n" + ex.Message, "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        finally
+                        {
+
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Operação cancelada!", "Cancelado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                //linkar métodos para clonar frames
+                dispositivo.NewFrame += NovoFrame;
+            }
+            else
+            {
+                MessageBox.Show("O dispositivo não foi iniciado!", "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
